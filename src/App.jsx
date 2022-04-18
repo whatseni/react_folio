13294a756511a70d7f0292c2709964b48184e71e
siha <es2008e@naver.com>
2022-04-18 19:20:31 +0900

    file structure add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+import Header from "./components/header/Header";
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
