ce2ed8c75c33a54c3f09c6726f6e44840d45f432
siha <es2008e@naver.com>
2022-04-18 21:57:15 +0900

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,103 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
+
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>WEB DEVELOPMENT</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creattion</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>abcdeddddddddddddddd</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
